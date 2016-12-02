c99a7076fba79b2a870e9bd084fe2ab2eb7846b9
Alessandro Barzanti <a.barzanti@gmail.com>
2016-12-02 15:25:54 +0000

    Added TryCatchStrategy

---
+++ src/Strategy/TryCatch.php
@@ -0,0 +1,36 @@
+<?php
+namespace ScriptFUSION\Mapper\Strategy;
+
+use ScriptFUSION\Mapper\Mapping;
+
+/**
+ * TryCatch uses the handler callback to catch and manage any exception thrown by
+ * the primary strategy, if an exception was raised delegates to a fallback expression.
+ */
+class TryCatch extends Decorator
+{
+    private $expression;
+    private $handler;
+
+    /**
+     * @param Strategy $strategy
+     * @param callable $handler
+     * @param Strategy|Mapping|array|mixed $expression
+     */
+    public function __construct(Strategy $strategy, callable $handler, $expression)
+    {
+        parent::__construct($strategy);
+        $this->handler = $handler;
+        $this->expression = $expression;
+    }
+
+    public function __invoke($data, $context = null)
+    {
+        try {
+            return parent::__invoke($data, $context);
+        } catch (\Exception $e) {
+            call_user_func($this->handler, $e);
+            return $this->delegate($this->expression, $data, $context);
+        }
+    }
+}
---
+++ test/Integration/Mapper/Strategy/TryCatchTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace ScriptFUSIONTest\Unit\Mapper\Strategy;
+
+use ScriptFUSION\Mapper\Mapper;
+use ScriptFUSION\Mapper\Strategy\Callback;
+use ScriptFUSION\Mapper\Strategy\TryCatch;
+
+final class TryCatchTest extends \PHPUnit_Framework_TestCase
+{
+    private $callback;
+
+    protected function setUp()
+    {
+        $this->callback = new Callback(function ($data) {
+            if ($data[0] === 'LogicError') {
+                throw new \LogicException('Test Exception');
+            }
+            if ($data[0] === 'DomainError') {
+                throw new \DomainException('Test Exception');
+            }
+            return $data;
+        });
+
+    }
+    public function testTryCatch()
+    {
+        /** @var TryCatch $tryCatch */
+        $tryCatch = (
+            new TryCatch(
+                $this->callback,
+                function ($e) {},
+                'ExceptionHandled'
+            )
+        )->setMapper(new Mapper);
+        self::assertSame(['foo','bar'], $tryCatch(['foo','bar']));
+        self::assertSame('ExceptionHandled', $tryCatch(['DomainError','foo']));
+    }
+
+    public function testMultipleTryCatch()
+    {
+        /** @var TryCatch $tryCatch */
+        $tryCatch = (
+        new TryCatch(
+            new TryCatch(
+                $this->callback,
+                function ($e) {
+                    if (get_class($e) === \DomainException::class) {
+                        throw $e;
+                    }
+                },
+                'LogicExceptionHandled'
+            ),
+            function ($e) {},
+            'DomainExceptionHandled'
+        )
+        )->setMapper(new Mapper);
+        self::assertSame(['foo','bar'], $tryCatch(['foo','bar']));
+        self::assertSame('LogicExceptionHandled', $tryCatch(['LogicError','foo']));
+        self::assertSame('DomainExceptionHandled', $tryCatch(['DomainError','foo']));
+    }
+}
